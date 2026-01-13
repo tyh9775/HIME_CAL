3af82f6be2069cedbda829cac7d4d4b163eaaa11
Teajun Kim <tyh0249@wmich.edu>
2026-01-13 13:29:14 -0500

    code

--- full_cal_old.sh
+++ full_cal_old.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-#this is the shell for running the full calibration process
-#Ultimately want to get Energy from Time over Threshold (ToT)
-#Energy=c*Amplitude+d
-#Amplitude=A*exp(k*ToT)+B
-
-
 set -e
 
-#switches for running different parts of the code
-original=false #run Kin's original code
-mu_st=true #load and combine cosmic muon data
-single=true #run the single detector estimation
+all=1 #switch between using just june data (0) or all data (1)
+original=0 #run Kin's original code
+mu_st=0
+limit=true
+single=0 #run the single detector estimation
 load=0 #for loading given data for calibration
 cal=0 #for applying the calibration parameters
 recut=0 #for automatically remaking tcutg files 
@@ -20,16 +15,11 @@ ecal=0 #creating E vs tot graphs
 vis=0
 muon=0
 veto=0
-read=1
 
-
-#switches for different output options
-limit=true
 Hel=false
 E_min=-1 # neg = don't use; zero = use val in myConst; pos = use given value
+read=1
 
-
-#functions####################################################################################
 #function to pull out parameters from txt file
 get_params() {
     local det_line="$1"
@@ -57,11 +47,17 @@ get_double() {
     grep -E "double[[:space:]]+${name}[[:space:]]*\{" "$header" \
       | sed -E 's/.*\{([^}]+)\}.*/\1/'
 }
-##############################################################################################
 
+cut_file1="./TCuts/cuts_L0.root"
+cut_file2="./TCuts/cuts_L1.root"
+cut_file3="./TCuts/cuts_L2.root"
 
-result_dir="tot_cal_results"
+result_dir="sb_results"
 mkdir -p $result_dir
+amp_dir_0="amp_results_v3"
+mkdir -p $amp_dir_0
+
+amp_dir=$amp_dir_0
 
 En=$E_min
 vis_res="${amp_dir_0}/vis"
@@ -167,11 +163,12 @@ then
     vis_res_2="${vis_res_2}_lim"
 fi
 
+
 runs23=( 1148 1149 1150 1151 1152 1153 1154 1155 1156 1158 1160 1161 1162 1163 1164 1165 1166 1167 1168 1169 1170 1171 1172 1174 1175 1176 1178 1179 1180 1181 1182 1183 1184 1185 1186 1189 1190 1191 1192 1195 1196 1197 1198 1199 1200 1201 1202 1203 1204 1205 1206 1207 1208 1210 )
 
 runs34=( 1211 1212 1213 1214 1215 1217 1218 1219 1221 1222 1223 1225 1226 1227 1232 1234 1235 1236 1239 1240 1241 1242 1243 1244 1245 )
 
-run34_nov=(  1644  1645  1646  1647  1648  1649  1650  1651  1652  1653  1654  1655  1656  1657  1658  1659  1660  1661  1662  1663  1664  1665  1666  1667  1668  1669  1670  1671  1672  1673  1675  1676  1677  1679  1680  1681  1682  1683  1684  1685  1686  1687  1688  1689  1690  1691  1692  1693  1694  1695  1696  1697  1698  1699  1700  1701  1702  1703  1704  1705  1706  1707  1708  1709  1710  1711  1721  1729  1730  1731  1732  1733  1734  1735  1736  1737  1738  1739  1740  1741  1742  1743  1744  1745  1746  1747  1748  1750  1751  1752  1753  1754  1755  1756  1757  1758  1759  1760  1761  1762  1763  1764  1765  1766  1767  1768  1769  1770  1771  1772  1773  1774  1775  1776  1777  1778  1779  1780  1781  1782  1783  1784  1785  1786  1788  1790  1791  1792  1793  1794  1800  1801  1802  1803  1804  1805  1806  1807  1808  1809  1810  1811  1812  1813  1814  1815  1816  1817  1818  1819  1822  1823  1824  1825  1826  1827  1828  1829  1830  1831  1832  1833  1834  1835  1836  1837  1838  1839  1840  1841  1842  1843  1844  1845  1846  1847  1848  1849  1850  1851  1852  1853  1854  1855  1856  1857  1858  1859  1860  1861  1862  1863  1864  1865  1866  1867  1868  1869  )
+run34_nov=( 1644  1645  1646  1647  1648  1649  1650  1651  1652  1653  1654  1655  1656  1657  1658  1659  1660  1661  1662  1663  1664  1665  1666  1667  1668  1669  1670  1671  1672  1673  1675  1676  1677  1679  1680  1681  1682  1683  1684  1685  1686  1687  1688  1689  1690  1691  1692  1693  1694  1695  1696  1697  1698  1699  1700  1701  1702  1703  1704  1705  1706  1707  1708  1709  1710  1711  1721  1729  1730  1731  1732  1733  1734  1735  1736  1737  1738  1739  1740  1741  1742  1743  1744  1745  1746  1747  1748  1750  1751  1752  1753  1754  1755  1756  1757  1758  1759  1760  1761  1762  1763  1764  1765  1766  1767  1768  1769  1770  1771  1772  1773  1774  1775  1776  1777  1778  1779  1780  1781  1782  1783  1784  1785  1786  1788  1790  1791  1792  1793  1794  1800  1801  1802  1803  1804  1805  1806  1807  1808  1809  1810  1811  1812  1813  1814  1815  1816  1817  1818  1819  1822  1823  1824  1825  1826  1827  1828  1829  1830  1831  1832  1833  1834  1835  1836  1837  1838  1839  1840  1841  1842  1843  1844  1845  1846  1847  1848  1849  1850  1851  1852  1853  1854  1855  1856  1857  1858  1859  1860  1861  1862  1863  1864  1865  1866  1867  1868  1869 )
 
 # shadow bar 4, at bottom, backward angle
 runs4a=(1247 1248 1251 1252 1253 1254 1255 1256 1261 1262 1263 1264 1265 1266 1268 1269 1273 1274 1275 1276 1277 1279 1280 1281 1282 1283 1284 1285 1286 1287 1288 1289 1290 1291 1292 1293 1294 1295 1296 1297 1298 1299)
@@ -179,30 +176,58 @@ runs4a=(1247 1248 1251 1252 1253 1254 1255 1256 1261 1262 1263 1264 1265 1266 12
 # shadow bar 4, at bottom, forward angle, but target switched to Xe132
 runs4b=(1300 1304 1305 1306 1308 1309 1310 1311 1312 1313 1314 1315 1316 1317 1318 1319 1320 1321 1322 1324 1325 1326 1327 1331)
 
-runs_test=( 1870 1871 1872 1873 1874 1875 1876 1877 1878 1879 1880 1881 1882 1883 1884 1885 1886 1887 1888 1889 1890 1891 1892 1893 1894 1895 1896 1897 1898 1899 1900 1901 1902 1903 1904 1905 1906 1907 1908 1909 1910 1911 1912 1913 1914 1915 1916 )
-#runs_t=(${runs_test[@]})
-#runs_t=$( IFS=, ; echo "${runs_t[*]}" )
+#every_run=( ${runs23[@]} ${runs34[@]} ${runs4a[@]} ${runs4b[@]} ${run34_nov[@]} )
+runs_june=( ${runs23[@]} ${runs34[@]} ${runs4a[@]} ${runs4b[@]} )
+runs_nov=( ${run34_nov[@]} )
+runs_all=( ${runs_june[@]} ${runs_nov[@]} )
 
-runs_23=( ${runs23[@]} )
-runs_34=( ${runs34[@]} )
-#runs_34n=( ${run34_nov[@]} )
-runs_34n=( ${run34_nov[@]} ${runs_test[@]} )
-runs_4=( ${runs4a[@]} ${runs4b[@]} )
+runs_j_2=( ${runs34[@]} ${runs4a[@]} ${runs4b[@]} ) #june run without sb 2
+runs_j_3=( ${runs4a[@]} ${runs4b[@]} ) #june run without sb 3
+runs_j_4=( ${runs23[@]} ) #june run without sb 4
 
-runs_23=$( IFS=, ; echo "${runs_23[*]}" )
-runs_34=$( IFS=, ; echo "${runs_34[*]}" )
-runs_34n=$( IFS=, ; echo "${runs_34n[*]}" )
-runs_4=$( IFS=, ; echo "${runs_4[*]}" )
+runs_a_2=( ${runs34[@]} ${runs4a[@]} ${runs4b[@]} ${run34_nov[@]} ) #all run w/o sb 2
+runs_a_3=( ${runs4a[@]} ${runs4b[@]} ) #all run w/o sb 3
+runs_a_4=( ${runs23[@]} ) #all run w/o sb4
 
-runs_34t=( ${runs34[@]} ${run34_nov[@]} ${runs_test[@]} )
-runs_34t=$( IFS=, ; echo "${runs_34t[*]}" )
+runs_j_2_inc=( ${runs23[@]} ) #june run including sb 2
+runs_j_3_inc=( ${runs23[@]} ${runs34[@]} ) #june run inc sb 3
+runs_j_4_inc=( ${runs34[@]} ${runs4a[@]} ${runs4b[@]} ) #june run inc sb 4
 
-#runs_all=( ${runs23[@]} ${runs34[@]} ${run34_nov[@]} ${runs4a[@]} ${runs4b[@]} )
-runs_all=( ${runs23[@]} ${runs34[@]} ${run34_nov[@]} ${runs_test[@]} ${runs4a[@]} ${runs4b[@]} )
+runs_a_2_inc=( ${runs23[@]} )
+runs_a_3_inc=( ${runs23[@]} ${runs34[@]} ${run34_nov[@]} )
+runs_a_4_inc=( ${runs34[@]} ${runs4a[@]} ${runs4b[@]} ${run34_nov[@]} ) 
 
+
+runs_june=$( IFS=, ; echo "${runs_june[*]}" )
+runs_nov=$( IFS=, ; echo "${runs_nov[*]}" )
 runs_all=$( IFS=, ; echo "${runs_all[*]}" )
 
+runs_j_2=$( IFS=, ; echo "${runs_j_2[*]}" )
+runs_j_3=$( IFS=, ; echo "${runs_j_3[*]}" )
+runs_j_4=$( IFS=, ; echo "${runs_j_4[*]}" )
 
+runs_a_2=$( IFS=, ; echo "${runs_a_2[*]}" )
+runs_a_3=$( IFS=, ; echo "${runs_a_3[*]}" )
+runs_a_4=$( IFS=, ; echo "${runs_a_4[*]}" )
+
+runs_j_2_inc=$( IFS=, ; echo "${runs_j_2_inc[*]}" )
+runs_j_3_inc=$( IFS=, ; echo "${runs_j_3_inc[*]}" )
+runs_j_4_inc=$( IFS=, ; echo "${runs_j_4_inc[*]}" )
+
+runs_a_2_inc=$( IFS=, ; echo "${runs_a_2_inc[*]}" )
+runs_a_3_inc=$( IFS=, ; echo "${runs_a_3_inc[*]}" )
+runs_a_4_inc=$( IFS=, ; echo "${runs_a_4_inc[*]}" )
+
+
+
+runs=$runs_june
+runs_2=$runs_j_2 #runs without sb 2
+runs_3=$runs_j_3 #runs without sb 3
+runs_4=$runs_j_4  #runs without sb 4
+
+runs_2_inc=$runs_j_2_inc #runs inc sb 2
+runs_3_inc=$runs_j_3_inc #runs inc sb 3
+runs_4_inc=$runs_j_4_inc  #runs inc sb 4
 
 if [ $all == 1 ]
 then
---
+++ myConst.h
@@ -0,0 +1,218 @@
+#ifndef MYCONST_H
+#define MYCONST_H
+#include <cmath>
+#include <iostream>
+#include <list>
+#include <vector>
+#include <unordered_map>
+#include <tuple>
+#include <string>
+#include <fstream>
+#include <regex>
+#include <algorithm>
+
+
+//particle masses in MeV/c^2 (from NIST reference)
+double pro_m {938.272};
+double neu_m {939.565};
+double deu_m {1875.612};
+double tri_m {2808.921};
+double hel_m {2808.391};
+double alp_m {3727.379};
+
+//distance to the detector in meters
+double det_dist {4.0};
+
+//time offset in ns
+double t_os {16.0};
+
+//punch through energies in BC400 scintillator (from Lisecute++) in MeV
+double pro_pt {47.971};
+double deu_pt {65.526};
+double tri_pt {78.672};
+double hel_pt {168.557};
+double alp_pt {191.353};
+
+double cos_mu {8.0};
+double mu_tot0 {21.0};
+double mu_tot1 {19.5};
+double mu_tot2 {20.5};
+
+//BC408 can detect x rays of energies <100keV (around 10% E res around here)
+double En_min {0.1};
+double En_min_err {0.01};
+
+double num_ent = 13215485.0;
+//punch though points (estimated by eye)
+/* std::vector<double> pro_0 {26.0,25.5};
+std::vector<double> deu_0 {33.2,26.5};
+std::vector<double> tri_0 {37.8,27.0};
+std::vector<double> hel_0 {21.6,30.0};
+std::vector<double> alp_0 {24.4,30.5}; 
+
+std::vector<double> pro_1 {21.8,24.0};
+std::vector<double> deu_1 {28.2,25.0};
+std::vector<double> tri_1 {33.4,25.5};
+std::vector<double> hel_1 {18.2,28.5};
+std::vector<double> alp_1 {20.6,29.0};
+
+std::vector<double> pro_2 {19.8,25.0};
+std::vector<double> deu_2 {25.8,25.5};
+std::vector<double> tri_2 {30.2,26.5};
+std::vector<double> hel_2 {16.2,29.0};
+std::vector<double> alp_2 {18.2,30.0};*/
+
+
+std::vector<double> pro_0 {25.4,25.5};
+std::vector<double> deu_0 {32.8,26.4};
+std::vector<double> tri_0 {37.8,27.1};
+std::vector<double> hel_0 {21.4,29.7};
+std::vector<double> alp_0 {23.8,30.8};
+
+std::vector<double> pro_1 {21.8,24.3};
+std::vector<double> deu_1 {28.4,25.5};
+std::vector<double> tri_1 {33.2,26.1};
+std::vector<double> hel_1 {18.0,28.5};
+std::vector<double> alp_1 {20.4,29.4};
+
+std::vector<double> pro_2 {19.6,25.0};
+std::vector<double> deu_2 {25.6,25.8};
+std::vector<double> tri_2 {30.2,26.7};
+std::vector<double> hel_2 {16.0,29.6};
+std::vector<double> alp_2 {18.4,30.1};
+
+std::vector<double> L0_tof {pro_0[0],deu_0[0],tri_0[0],hel_0[0],alp_0[0]};
+std::vector<double> L1_tof {pro_1[0],deu_1[0],tri_1[0],hel_1[0],alp_1[0]};
+std::vector<double> L2_tof {pro_2[0],deu_2[0],tri_2[0],hel_2[0],alp_2[0]};
+
+
+// (m/ns)^2 = (J/kg)/(10^-9)^2 = (6241509074461 MeV / 6.0221366516752E+26 u) * (10^18) = 10364.27673 MeV/u
+//conversion factor
+double cnv_fctr {10364.27673};
+
+////////
+//limit loaded histograms
+///////
+
+int hstart1 {2};
+int hstop1 {23};
+int hbase1 {14};
+int hstart2 {24};
+int hstop2 {47};
+int hbase2 {38};
+int hstart3 {48};
+int hstop3 {71};
+int hbase3 {62};
+
+
+//sections
+//{1,2,3,4,5,6,7};
+std::vector<int> sec_omit {1,6,7}; //change which sections to omit
+std::vector<int> sec_omit_r {1};
+std::vector<int> sec_omit_e {};
+
+int max_deg {2}; //for fitting polynomials
+int min_deg {1};
+
+//initial guess for a parameters (adjust to the parameters of the baseline detector)
+double a1 {0.5}; 
+double a2 {0.29};
+double ka {0.25};
+
+double E_A {0.02};
+double E_B {-0.01};
+double E_k {0.30};
+
+/* double a1 {0.5}; 
+double a2 {0.29};
+double ka {0.15}; */
+
+//for checking the improvement of statistics w/ the calibration method
+double xc1 {20.0};
+double xc2 {25.0};
+double xc3 {28.0};
+double xc4 {32.0};
+
+double yc1 {20.0};
+double yc2 {24.0};
+double yc3 {26.0};
+double yc4 {30.0};
+
+//detectors blocked completely by shadow bars
+std::vector<int> sb2 {6,41,54};
+std::vector<int> sb3 {17,41,65};
+std::vector<int> sb4 {17,30,65};
+
+//positions of the shadow bars
+std::vector<double> sb2_a {-184.0,181.0};
+std::vector<double> sb2_b {-184.0,274.0};
+std::vector<double> sb2_c {-278.0,274.0};
+std::vector<double> sb2_d {-277.0,181.0};
+
+double sb2_xmin {-277.5};
+double sb2_xmax {-184.0};
+double sb2_ymin {181.0};
+double sb2_ymax {274.0};
+
+std::vector<double> sb3_a {-181.0,-276.0};
+std::vector<double> sb3_b {-181.0,-184.0};
+std::vector<double> sb3_c {-280.0,-184.0};
+std::vector<double> sb3_d {-280.0,-277.0};
+
+double sb3_xmin {-280.0};
+double sb3_xmax {-181.0};
+double sb3_ymin {-276.5};
+double sb3_ymax {-184.0};
+
+std::vector<double> sb4_a {277.0,-279.0};
+std::vector<double> sb4_b {276.0,-181.0};
+std::vector<double> sb4_c {184.0,-181.0};
+std::vector<double> sb4_d {185.0,-280.0};
+
+double sb4_xmin {184.0};
+double sb4_xmax {276.5};
+double sb4_ymin {-279.5};
+double sb4_ymax {-181.0};
+
+//vw edges
+std::vector<int> v0l0 {2,7};
+std::vector<int> v1l0 {7,16};
+std::vector<int> v2l0 {16,23};
+
+std::vector<double> v0l1 {155.0,500.0};
+std::vector<double> v1l1 {-185.0,195.0};
+std::vector<double> v2l1 {-500.0,-165.0};
+
+std::vector<int> v0l2 {48,55};
+std::vector<int> v1l2 {55,64};
+std::vector<int> v2l2 {64,71};
+
+
+std::vector<int> v0l1d {26,30};
+std::vector<int> v1l1d {30,39};
+std::vector<int> v2l1d {39,56};
+
+//tot threshold for wc
+double my_tot_thresh_wc = 21.233;
+
+//tot threshold for bg
+double my_tot_thresh = 18.0;
+
+//rebin factor
+int rbf {2};
+
+
+int nbins_hitpattern {200}; //default 200
+
+
+double z_target_jun = -21.86;
+double z_target_nov = -32.8;
+
+double z_sig_jun = 2.15;
+double z_sig_nov = 2.5;
+
+
+std::vector<int> nov_runs = {1644,1645,1646,1647,1648,1649,1650,1651,1652,1653,1654,1655,1656,1657,1658,1659,1660,1661,1662,1663,1664,1665,1666,1667,1668,1669,1670,1671,1672,1673,1675,1676,1677,1679,1680,1681,1682,1683,1684,1685,1686,1687,1688,1689,1690,1691,1692,1693,1694,1695,1696,1697,1698,1699,1700,1701,1702,1703,1704,1705,1706,1707,1708,1709,1710,1711,1721,1729,1730,1731,1732,1733,1734,1735,1736,1737,1738,1739,1740,1741,1742,1743,1744,1745,1746,1747,1748,1750,1751,1752,1753,1754,1755,1756,1757,1758,1759,1760,1761,1762,1763,1764,1765,1766,1767,1768,1769,1770,1771,1772,1773,1774,1775,1776,1777,1778,1779,1780,1781,1782,1783,1784,1785,1786,1788,1790,1791,1792,1793,1794,1800,1801,1802,1803,1804,1805,1806,1807,1808,1809,1810,1811,1812,1813,1814,1815,1816,1817,1818,1819,1822,1823,1824,1825,1826,1827,1828,1829,1830,1831,1832,1833,1834,1835,1836,1837,1838,1839,1840,1841,1842,1843,1844,1845,1846,1847,1848,1849,1850,1851,1852,1853,1854,1855,1856,1857,1858,1859,1860,1861,1862,1863,1864,1865,1866,1867,1868,1869,};
+
+
+#endif
